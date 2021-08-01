ad89f19a9362e5cbdb0666e4b74bc7dbae792fd8
Ban Finch <rheriel@hotmail.co.jp>
2021-07-31 20:21:13 -0500

    Create tcpClient.py and add more details on README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# BlackHatPython
+# Black Hat Python Code Samples 
+
 Python samples from the book by Justin Seitz.
+
+# Running Samples
+
+The code samples in the book use Python 2.7
--- tcpClient.py
+++ tcpClient.py
@@ -1,1 +1,18 @@
-tcpClient.py
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+# Create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client
+client.connect((target_host, target_port))
+
+# Send data
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# Receive data
+response = client.recv(4096)
+
+print response
